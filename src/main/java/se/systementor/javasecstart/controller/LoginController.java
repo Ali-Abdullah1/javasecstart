0ae58694073858294a0f48832a8e04090f52d5ca
Farzad <farzad_jalili@hotmail.se>
2024-08-26 00:02:44 +0200

    v2

--- src/main/java/se/systementor/javasecstart/controller/LoginController.java
+++ src/main/java/se/systementor/javasecstart/controller/LoginController.java
@@ -1,27 +1,14 @@
 package se.systementor.javasecstart.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(Model model) {
-        model.addAttribute("activeFunction", "login");
-        return "login";
-    }
 
-    @PostMapping("/login")
-    public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
-        if ("admin".equals(username) && "password".equals(password)) {
-            return "redirect:/admin/dogs";
-        } else {
-            model.addAttribute("error", "Fel användarnamn eller lösenord.");
-            return "login";
-        }
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "/login";
     }
 }
---
+++ src/main/java/se/systementor/javasecstart/security/UserDataSeeder.java
@@ -0,0 +1,34 @@
+package se.systementor.javasecstart.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDataSeeder {
+    @Autowired
+    UserRepo userRepository;
+
+
+    public void seedUsers() {
+        if (userRepository.getByUsername("farzad.jalili@hotmail.se") == null) {
+            addUser("farzad.jalili@hotmail.se", "farzad", "1");
+        }
+        if (userRepository.getByUsername("ahnaf.khan@hotmail.se") == null) {
+            addUser("ahnaf.khan@hotmail.se", "Aahnaf", "2");
+        }
+        if (userRepository.getByUsername("alina.mazic@hotmail.se") == null) {
+            addUser("alina.mazic@hotmail.se", "alina", "3");
+        }
+    }
+
+    private void addUser(String mail,String firstName, String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hash = encoder.encode(password);
+        User user = User.builder().enabled(true).password(hash).username(mail).firstName(firstName).build();
+        userRepository.save(user);
+    }
+
+}
