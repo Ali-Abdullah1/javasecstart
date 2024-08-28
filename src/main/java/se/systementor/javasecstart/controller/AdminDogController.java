1f5b45c424b2519754ae57067aee292fed9ced59
alexanderp9 <alexander.pino@hotmail.com>
2024-08-28 09:41:07 +0200

    edit dogs.

--- src/main/java/se/systementor/javasecstart/controller/AdminDogController.java
+++ src/main/java/se/systementor/javasecstart/controller/AdminDogController.java
@@ -2,22 +2,50 @@ package se.systementor.javasecstart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import se.systementor.javasecstart.model.Dog;
 import se.systementor.javasecstart.services.DogService;
+
+import java.util.Optional;
 
 @Controller
 public class AdminDogController {
     @Autowired
     private DogService dogService;
+
     @GetMapping(path="/admin/dogs")
     String list(Model model){
         model.addAttribute("activeFunction", "home");
-//        setupVersion(model);
-
         model.addAttribute("dogs", dogService.getPublicDogs());
         return "admin/dogs/list";
     }
 
+    @GetMapping(path="/admin/dogs/edit/{id}")
+    public String editdogs(@PathVariable("id") int id, Model model) {
+        Optional<Dog> dog = dogService.getDogById(id);
+        if (dog.isPresent()) {
+            model.addAttribute("dog", dog.get());
+            return "admin/dogs/edit";
+        } else {
+            return "redirect:/admin/dogs";
+        }
+    }
+
+    @PostMapping(path="/admin/dogs/update/{id}")
+    public String changeDog(@PathVariable("id") int id, @ModelAttribute Dog dog) {
+        Optional<Dog> existingDog = dogService.getDogById(id);
+        if (existingDog.isPresent()) {
+            Dog updatedDog = existingDog.get();
+            updatedDog.setName(dog.getName());
+            updatedDog.setBreed(dog.getBreed());
+            updatedDog.setAge(dog.getAge());
+            updatedDog.setSize(dog.getSize());
+            updatedDog.setPrice(dog.getPrice());
+            dogService.saveDog(updatedDog);
+            return "redirect:/admin/dogs";
+        } else {
+            return "redirect:/admin/dogs";
+        }
+    }
 }
--- src/main/java/se/systementor/javasecstart/services/DogService.java
+++ src/main/java/se/systementor/javasecstart/services/DogService.java
@@ -1,18 +1,28 @@
-package se.systementor.javasecstart.services;
+    package se.systementor.javasecstart.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import se.systementor.javasecstart.model.Dog;
-import se.systementor.javasecstart.model.DogRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import se.systementor.javasecstart.model.Dog;
+    import se.systementor.javasecstart.model.DogRepository;
 
-import java.util.List;
+    import java.util.List;
+    import java.util.Optional;
 
-@Service
-public class DogService {
-    @Autowired
-    DogRepository dogRepository;
+    @Service
+    public class DogService {
+        @Autowired
+        DogRepository dogRepository;
 
-    public List<Dog> getPublicDogs(){
-        return dogRepository.findAllBySoldToIsNull();
+        public List<Dog> getPublicDogs(){
+            return dogRepository.findAllBySoldToIsNull();
+        }
+
+        public Optional<Dog> getDogById(int id) {
+            return dogRepository.findById((long) id);
+        }
+
+        public void saveDog(Dog dog) {
+            dogRepository.save(dog);
+        }
+
     }
-}
